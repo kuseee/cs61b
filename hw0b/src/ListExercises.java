a9d10fff09c7daccb95f3d580f0a3915c0221e56
kuse <2573405476@qq.com>
2024-07-26 18:48:19 +0800

    hm0b

--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -1,29 +1,52 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListExercises {
 
     /** Returns the total sum in a list of integers */
 	public static int sum(List<Integer> L) {
-        // TODO: Fill in this function.
-        return 0;
+        int sum = 0;
+        for(int i:L){
+            sum += i;
+        }
+        return sum;
     }
 
     /** Returns a list containing the even numbers of the given list */
     public static List<Integer> evens(List<Integer> L) {
-        // TODO: Fill in this function.
-        return null;
+        List<Integer> L2 = new ArrayList<>();
+        for (int i:L){
+            if(i % 2 == 0){
+                L2.add(i);
+            }
+        }
+        return L2;
     }
 
     /** Returns a list containing the common item of the two given lists */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
-        // TODO: Fill in this function.
-        return null;
+        List<Integer> L3 = new ArrayList<>();
+        for (int i:L1){
+            for (int j : L2){
+                if(i == j && !L3.contains(i)){
+                    L3.add(i);
+                }
+            }
+        }
+        return L3;
     }
 
 
     /** Returns the number of occurrences of the given character in a list of strings. */
     public static int countOccurrencesOfC(List<String> words, char c) {
-        // TODO: Fill in this function.
-        return 0;
+        int count = 0;
+        for (String x: words){
+            for(int i=0;i < x.length();i++){
+                if (x.charAt(i) == c){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,26 +1,52 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+
+
+//map与list的创建方式十分相似，都是***<**> name = new ***<>();
+
 
 public class MapExercises {
     /** Returns a map from every lower case letter to the number corresponding to that letter, where 'a' is
      * 1, 'b' is 2, 'c' is 3, ..., 'z' is 26.
      */
     public static Map<Character, Integer> letterToNum() {
-        // TODO: Fill in this function.
-        return null;
+        Map<Character,Integer> alphabet = new HashMap<>();
+        for(int i = 97;i<123;i++){
+            alphabet.put((char)i,i-96);
+        }
+        return alphabet;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
      *  is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
-        // TODO: Fill in this function.
-        return null;
+        Map<Integer,Integer> pow = new HashMap<>();
+        for (int i: nums){
+            pow.put(i,i*i);
+        }
+        return pow;
     }
+
+
+
+
+
+    //map的改值只需要输入原有键+新值即可
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
-        // TODO: Fill in this function.
-        return null;
+        Map<String,Integer> count = new HashMap<>();
+        for(String str:words){
+            if(count.containsKey(str)){
+                count.put(str,count.get(str)+1);
+            }
+            else{
+                count.put(str,1);
+            }
+        }
+        return count;
     }
 }
