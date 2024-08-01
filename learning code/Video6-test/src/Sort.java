55a5a3314362c35979ae5929c6e1a998f6b2344a
kuse <2573405476@qq.com>
2024-08-01 11:24:42 +0800

    7/31

--- learning code/Video6-test/src/Sort.java
+++ learning code/Video6-test/src/Sort.java
@@ -1,5 +1,36 @@
 public class Sort {
-    public static void sort(String[] x){
+    public static void sort(String[] input){
+        sort(input,0);
         return;
+    }
+
+
+//    这个递归有技巧的，因为正常没必要给个start，默认从0开始就行，但是start为递归创造了条件
+    private static void sort(String[] input, int start){
+        if(start == input.length - 1){
+            return;
+        }
+        swap(input,findsmallest(input,start),start);
+        sort(input,start + 1);
+    }
+
+    public static int findsmallest(String[] input,int start){
+        String smallest = input[start];
+        int sma = start;
+        for(int i = start; i < input.length; i++){
+            if(smallest.compareTo(input[i])>0){
+                smallest = input[i];
+                sma = i;
+            }
+        }
+        return sma;
+    }
+
+    public static String[] swap(String[] input,int x, int y){
+        String temp;
+        temp = input[x];
+        input[x] = input[y];
+        input[y] = temp;
+        return input;
     }
 }
--- learning code/Video6-test/src/TestSort.java
+++ learning code/Video6-test/src/TestSort.java
@@ -11,4 +11,30 @@ public class TestSort {
 
         assertThat(input).isEqualTo(expected);
     }
+
+    @Test
+    public void TestTheSmallest(){
+        String[] input ={"yaya","a","guig","nmhlhh"};
+        int expected = 1;
+        int result = Sort.findsmallest(input,0);
+        assertThat(result).isEqualTo(expected);
+
+        expected = 1;
+        result = Sort.findsmallest(input,1);
+        assertThat(result).isEqualTo(expected);
+
+        expected = 2;
+        result = Sort.findsmallest(input,2);
+        assertThat(result).isEqualTo(expected);
+
+    }
+
+    @Test
+    public void TestSwap(){
+        String[] input ={"yaya","a","guig","nmhlhh"};
+        String[] expected ={"yaya","guig","a","nmhlhh"};
+        String[] result = Sort.swap(input,1,2);
+        assertThat(result).isEqualTo(expected);
+    }
 }
+
